87da550542da2d520e752d6de56e4ba8b5125eb9
Shohrukh Koyirov <shohrukhkoyirov@gmail.com>
2024-09-09 15:43:21 +0200

    initial message

---
+++ src/main/java/com/example/restfulwebservicespring/RESTfulWebServiceSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfulwebservicespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RESTfulWebServiceSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RESTfulWebServiceSpringApplication.class, args);
+	}
+
+}
